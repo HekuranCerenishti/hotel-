7e9b5ea02181204c6191be7ac6cfc13729b2af7d
HekuranCerenishti <cerenishtiani@gmail.com>
2024-08-25 11:52:09 +0200

    typo correction

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -9,7 +9,7 @@ const SearchItem = () => {
                 className="siImg" 
             />
             <div className="siDesc">
-                <h1 className="siTitle">Piazza Luxuary</h1>
+                <h1 className="siTitle">Piazza Luxury</h1>
                 <span className="siDistance">500m from center</span>
                 <span className="siTaxiOp">Free airport taxi</span>
                 <span className="siSubtitle">
